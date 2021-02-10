16267daec32f142c88066f6d82cde82edd85a9f5
Aysen <zeinell69@gmail.com>
2021-02-10 13:38:20 +0900

    auth screen upd

--- screens/AuthScreen.tsx
+++ screens/AuthScreen.tsx
@@ -13,7 +13,7 @@ export default function AuthScreen() {
   }
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Authorization</Text>
+      <Text style={styles.title}>Big Tic-Tac-Toe</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <SignInWithGoogle onSuccess={onSuccessSignIn} />
     </View>
